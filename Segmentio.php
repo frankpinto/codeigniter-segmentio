998e46f7537128cfc0771a85d6ce7cc5b1f2c0bc
Frank Pinto <frank.joseph.pinto@gmail.com>
2013-09-17 13:22:04 -0400

    Add ability to turn off tracking

--- Segmentio.php
+++ Segmentio.php
@@ -11,11 +11,16 @@ class Segmentio
 
   public $API_key;
   public $secret;
+  public $track;
 
-  public function __construct()
+  public function __construct($track = TRUE)
   {
     $this->CI =& get_instance();
     $this->input =& $this->CI->input;
+
+    $this->track = $track;
+    if (!$this->track)
+      return;
 
     $segment_config = $this->CI->config->item('segmentio');
     $this->API_key = $segment_config['API_key'];
@@ -30,41 +35,47 @@ class Segmentio
 
   public function identify($user_id, $traits = array(), $timestamp = NULL, $context = array())
   {
-    $phone = NULL;
-    if (isset($headers['X-MSISDN']) && $headers['X-MSISDN'])
-      $phone = $headers['X-MSISDN'];
-    elseif (isset($headers['X-WAP-Network-Client-MSISDN']) && $headers['X-WAP-Network-Client-MSISDN'])
-      $phone = $headers['X-WAP-Network-Client-MSISDN'];
-    
-    if (!isset($traits['phone']) && $phone)
-      $traits['phone'] = $phone;
+    if ($this->track)
+    {
+      $phone = NULL;
+      if (isset($headers['X-MSISDN']) && $headers['X-MSISDN'])
+        $phone = $headers['X-MSISDN'];
+      elseif (isset($headers['X-WAP-Network-Client-MSISDN']) && $headers['X-WAP-Network-Client-MSISDN'])
+        $phone = $headers['X-WAP-Network-Client-MSISDN'];
+      
+      if (!isset($traits['phone']) && $phone)
+        $traits['phone'] = $phone;
 
-    $context = array_merge($this->context, $context);
+      $context = array_merge($this->context, $context);
 
-    Analytics::identify($user_id, $traits, $timestamp, $context);
+      Analytics::identify($user_id, $traits, $timestamp, $context);
+    }
   }
 
   public function track($user_id, $event, $properties = NULL, $timestamp = NULL, $context = array())
   {
-    // Server-side page views
-    if ($event == 'Loaded a Page')
+    if ($this->track)
     {
-      $additional_properties = array('url' => $_SERVER['REQUEST_URI']);
-      if (isset($_SERVER['HTTP_REFERER']))
+      // Server-side page views
+      if ($event == 'Loaded a Page')
       {
-        $additional_properties['referer'] = $_SERVER['HTTP_REFERER'];
-        $additional_properties['referrer'] = $_SERVER['HTTP_REFERER'];
+        $additional_properties = array('url' => $_SERVER['REQUEST_URI']);
+        if (isset($_SERVER['HTTP_REFERER']))
+        {
+          $additional_properties['referer'] = $_SERVER['HTTP_REFERER'];
+          $additional_properties['referrer'] = $_SERVER['HTTP_REFERER'];
+        }
+
+        if ($properties)
+          $properties = array_merge($properties, $additional_properties);
+        else
+          $properties = $additional_properties;
       }
 
-      if ($properties)
-        $properties = array_merge($properties, $additional_properties);
-      else
-        $properties = $additional_properties;
+      $context = array_merge($this->context, $context);
+
+      Analytics::track($user_id, $event, $properties, $timestamp, $context);
     }
-
-    $context = array_merge($this->context, $context);
-
-    Analytics::track($user_id, $event, $properties, $timestamp, $context);
   }
 }
 
